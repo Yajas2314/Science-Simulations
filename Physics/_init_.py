64a031edbd901c3d68e162b9eec56d56b7baabcf
Yajas2314 <yajasagrawal@gmail.com>
2025-06-02 15:51:57 +0530

    _init_.py

---
+++ Physics/_init_.py
@@ -0,0 +1,7 @@
+from . import Reflection
+from . import Refraction
+from . import Dispersion
+from . import Wave Optics
+from . import Ohm's Law
+from . import Projectile
+from . import Electrostatics
