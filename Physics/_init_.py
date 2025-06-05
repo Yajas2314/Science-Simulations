607ddb25f5cfe539c19b0e41f17e0dc373716bfd
Yajas2314 <yajasagrawal@gmail.com>
2025-06-05 21:06:27 +0530

    _init_.py

--- Physics/_init_.py
+++ Physics/_init_.py
@@ -1,6 +1,6 @@
 from physics import reflection
 from physics import refraction
-from  physics import dispersion
+from physics import dispersion
 from physics import wave_optics
 from physics import ohms_law
 from physics import projectile_motion
