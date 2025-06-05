ff2d0cbe08cf59b49821e7c8175fbd01ce84108e
Yajas2314 <yajasagrawal@gmail.com>
2025-06-05 21:28:05 +0530

    Delete Physics directory

--- Physics/_init_.py
+++
@@ -1,7 +0,0 @@
-from physics import reflection
-from physics import refraction
-from physics import dispersion
-from physics import wave_optics
-from physics import ohms_law
-from physics import projectile_motion
-from physics import electrostatics
